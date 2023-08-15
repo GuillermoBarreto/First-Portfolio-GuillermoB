ca3bef045e5a176e106e85a523b0a11c6daa6478
DevWilliam23 <124272218+GuillermoBarreto@users.noreply.github.com>
2023-08-15 13:24:33 -0700

    Update validate.js

---
+++ validate.js
@@ -0,0 +1,7 @@
+console.assert(JSON.stringify(movies) === moviesJSON, {
+    message: "WARNING - movies dataset has has been modified!!",
+  })
+  
+  console.assert(JSON.stringify(movieDetails) === movieDetailsJSON, {
+    message: "WARNING - movieDetails dataset has been modified!!",
+  })
